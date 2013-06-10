b6d07b5eb5736384817fbd01d00453f90b5673de
Hipex <hipexIO@gmail.com>
2013-06-10 13:24:37 +0200

    widget support and service compatibility

--- unify.sql
+++ unify.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 03, 2013 at 03:54 PM
+-- Generation Time: Jun 10, 2013 at 01:23 PM
 -- Server version: 5.5.31
 -- PHP Version: 5.4.15
 
@@ -31,7 +31,6 @@ CREATE TABLE IF NOT EXISTS `items` (
   `serviceID` int(11) DEFAULT NULL,
   `parameter` varchar(100) DEFAULT NULL,
   `parentID` int(11) DEFAULT NULL,
-  `doUpdate` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`itemID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -43,8 +42,9 @@ CREATE TABLE IF NOT EXISTS `items` (
 
 CREATE TABLE IF NOT EXISTS `services` (
   `serviceID` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) DEFAULT NULL,
-  `module` varchar(100) DEFAULT NULL,
+  `serviceTitle` varchar(100) DEFAULT NULL,
+  `serviceModule` varchar(100) DEFAULT NULL,
+  `parentServiceID` int(11) NOT NULL,
   PRIMARY KEY (`serviceID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
@@ -52,13 +52,83 @@ CREATE TABLE IF NOT EXISTS `services` (
 -- Dumping data for table `services`
 --
 
-INSERT INTO `services` (`serviceID`, `name`, `module`) VALUES
-(0, 'group', 'group'),
-(1, 'gmail', 'gmail'),
-(2, 'gcal appointment', 'gcal_appointment'),
-(3, 'gcal calendar', 'gcal_calendar'),
-(4, 'evernote notebook', 'evernote_notebook'),
-(5, 'evernote note', 'evernote_note');
+INSERT INTO `services` (`serviceID`, `serviceTitle`, `serviceModule`, `parentServiceID`) VALUES
+(0, 'group', 'group', 0),
+(1, 'gmail', 'gmail', 0),
+(2, 'gcal appointment', 'gcal_appointment', 3),
+(3, 'gcal calendar', 'gcal_calendar', 0),
+(4, 'evernote notebook', 'evernote_notebook', 0),
+(5, 'evernote note', 'evernote_note', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `widgets`
+--
+
+CREATE TABLE IF NOT EXISTS `widgets` (
+  `widgetID` int(11) NOT NULL AUTO_INCREMENT,
+  `widgettypeID` int(11) NOT NULL,
+  `parentID` int(11) NOT NULL,
+  `preferences` varchar(100) NOT NULL,
+  PRIMARY KEY (`widgetID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `WidgetToItem`
+--
+
+CREATE TABLE IF NOT EXISTS `WidgetToItem` (
+  `WTIID` int(11) NOT NULL AUTO_INCREMENT,
+  `widgetID` int(11) NOT NULL,
+  `itemID` int(11) NOT NULL,
+  PRIMARY KEY (`WTIID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `widgettypes`
+--
+
+CREATE TABLE IF NOT EXISTS `widgettypes` (
+  `widgettypeID` int(11) NOT NULL AUTO_INCREMENT,
+  `widgettypeModule` varchar(100) NOT NULL,
+  `widgettypeTitle` varchar(100) NOT NULL,
+  PRIMARY KEY (`widgettypeID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `widgettypes`
+--
+
+INSERT INTO `widgettypes` (`widgettypeID`, `widgettypeModule`, `widgettypeTitle`) VALUES
+(1, 'widgettype_agenda', 'agenda'),
+(2, 'widgettype_notebook', 'notebook');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `WidgettypeToService`
+--
+
+CREATE TABLE IF NOT EXISTS `WidgettypeToService` (
+  `WTSID` int(11) NOT NULL AUTO_INCREMENT,
+  `widgettypeID` int(11) NOT NULL,
+  `serviceID` int(11) NOT NULL,
+  PRIMARY KEY (`WTSID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `WidgettypeToService`
+--
+
+INSERT INTO `WidgettypeToService` (`WTSID`, `widgettypeID`, `serviceID`) VALUES
+(1, 1, 3),
+(2, 2, 4),
+(3, 1, 2);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
