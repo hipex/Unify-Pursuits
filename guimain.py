756bf95844b30e2b2f2ec4d7e3c05301ed78c693
Hipex <hipexIO@gmail.com>
2013-06-21 12:37:18 +0200

    started with tkinter

--- guimain.py
+++ guimain.py
@@ -3,8 +3,11 @@
 import os
 import wx
 import MySQLdb as mdb
-import sys
+
 from StringIO import StringIO
+from Tkinter import *
+import tkSimpleDialog
+
 
 from functions import *
 import modules
@@ -15,279 +18,137 @@ mdb.con.autocommit(True)
 
 old_stdout = sys.stdout
 
+class gotoDialog(tkSimpleDialog.Dialog):
+	def body(self, master):
+		Label(master, text="Value").pack()
+		
+		self.title("goto dialog")
+		
+		self.e = Entry(master)
+		self.e.pack(padx=5)
+		
+		return self.e
+		
+	def apply(self):
+		value = self.e.get()
+		self.result = value
 
-CURRENTitem = [0, "none"]
-virtualitems = []
-
-class gotoDialog(wx.Dialog):
- 	def __init__(self, parent, id, title):
-		wx.Dialog.__init__(self, parent, id, title, size=(350,300))
+class addItemDialog(tkSimpleDialog.Dialog):
+	def body(self, master):
+		Label(master, text="add item").pack()
 		
-		hbox = wx.BoxSizer(wx.VERTICAL)
-		self.boxGo = wx.TextCtrl(self, -1)
-		btnGo = wx.Button(self, -1, 'go')
-		hbox.Add(self.boxGo)
-		hbox.Add(btnGo)
 		
-		self.SetSizer(hbox)
-		
-		btnGo.Bind(wx.EVT_BUTTON, self.OnClose)
-		
-	def OnClose(self, e):
-		self.Destroy()
 
 
-class Unify(wx.Frame):
-	
-	def __init__(self, *args, **kwargs):
-		super(Unify, self).__init__(*args, **kwargs)
-		
-		self.InitUI()
-	
-	def InitUI(self):
-		menubar= wx.MenuBar()
-		
-		mainMenu = wx.Menu()
-		
-		mainGoto = mainMenu.Append(wx.ID_ANY, 'Goto..', 'Goto an item page')
-		mainUp = mainMenu.Append(wx.ID_ANY, 'Go up', 'Move an item up')
-		mainReload = mainMenu.Append(wx.ID_ANY, 'Reload', 'reload current page')
-		mainQuit = mainMenu.Append(wx.ID_EXIT, 'Quit', 'Quit application')
-		
-		self.Bind(wx.EVT_MENU, self.goto, mainGoto)
-		self.Bind(wx.EVT_MENU, self.goUp, mainUp)
-		self.Bind(wx.EVT_MENU, self.doReload, mainReload)
-		self.Bind(wx.EVT_MENU, self.doQuit, mainQuit)
-				
-		menubar.Append(mainMenu, '&Navigation')
-		
-		itemMenu = wx.Menu()
-		itemAdd = itemMenu.Append(wx.ID_ANY, 'add', 'add an item')
-		itemRemove = itemMenu.Append(wx.ID_ANY, 'remove', 'remove an item')
-		itemCheck = itemMenu.Append(wx.ID_ANY, 'check', 'check items for existence')
-		
-		self.Bind(wx.EVT_MENU, self.itemAdd, itemAdd)
-		self.Bind(wx.EVT_MENU, self.itemRemove, itemRemove)
-		self.Bind(wx.EVT_MENU, self.itemCheck, itemCheck)
-		
-		menubar.Append(itemMenu, '&item')
-		
-		self.SetMenuBar(menubar)
-		
-		self.textplace = wx.TextCtrl(self, style= wx.TE_MULTILINE )
-		
-		self.SetSize((300,200))
-		self.SetTitle('Unify Pursuits')
-		self.Show(True)
-	
-	def goto(self, e):
-		global virtualitems
-		global CURRENTitem
+
+class App:
+	def __init__(self, master):
 		global mdb
-		global modules
-		
-		dia = gotoDialog(self, -1, 'buttons')
- 		res = dia.ShowModal()
-		answer = dia.boxGo.GetValue()
+		global modules		
 
-		dia.Destroy() 
-		outputty = StringIO()
-		sys.stdout = outputty
+		self.CURRENTitem = [0, "none"]
+		self.virtualitems = []
+		
+		self.master = master
+		
+		master.minsize(1000,450)
+		master.title("Unify Pursuits")
+		
+		frame = Frame(master)
+		frame.pack()
+		
+		# Initiate Menu
+		menubar = Menu(root)
+		
+		MNnavigate = Menu(menubar, tearoff=0)
+		MNnavigate.add_command(label="groups", command=self.showGroups)
+		MNnavigate.add_command(label="all", command=self.showAll)
+		MNnavigate.add_command(label="goto", command=self.showGoto)
+		MNnavigate.add_command(label="up", command=self.goUp)
+		MNnavigate.add_command(label="Quit", command=frame.quit)
+		menubar.add_cascade(label="navigate", menu=MNnavigate)
+		
+		MNitems = Menu(menubar, tearoff=0)
+		MNitems.add_command(label="add", command=self.addItem)
+		MNitems.add_command(label="remove", command=self.removeItem)
+		MNitems.add_command(label="update", command=self.updateItem)
+		menubar.add_cascade(label="items", menu=MNitems)
+		
+		MNwidgets = Menu(menubar, tearoff=0)
+		MNwidgets.add_command(label="add", command=self.addWidget)
+		MNwidgets.add_command(label="remove", command=self.removeWidget)
+		MNwidgets.add_command(label="update", command=self.updateWidget)
+		menubar.add_cascade(label="widgets", menu=MNwidgets)
+		
+		root.config(menu=menubar)
+		# Initiate Main
+		
+		
+	
+	def show(self):
+		
+		answer = showItem(mdb, modules, self.CURRENTitem, self.virtualitems)
+		result=answer[0]	
+		self.virtualitems = answer[1]
+		
+		if result == "error-item":
+			header(os, "this item does not exist")
+	
+	## navigate functions
+	def showGroups(self):
+		# show groups
+		showItemsAsTree(mdb, modules, 0, 0)
+			
+	def showAll(self):
+		# show all
+		showItemsAsTree(mdb, modules)
+	
+	
+	def showGoto(self):
+		dialog = gotoDialog(self.master, self.tkSimpleDialog.Dialog)
+		
+		answer = dialog.result
 		
 		# if answer starts with v:
 		if str(answer)[0] == 'v':
 			# requested virtual item
 			
 			# strip the v
-			CURRENTitem[1] = int(answer[1:]) 	
+			self.CURRENTitem[1] = int(answer[1:]) 
+		elif answer == 'c':
+			# show current
+			pass	
 			
 		else:
 			# requested database item
-			CURRENTitem[0] = answer
-			CURRENTitem[1] = "none"
+			self.CURRENTitem[0] = answer
+			self.CURRENTitem[1] = "none"
 		
-		answer = showItem(mdb, modules, CURRENTitem, virtualitems)
-
-		result=answer[0]
+		self.show()
 		
-		virtualitems = answer[1]
-
-		
-		if result == "error-item":
-			header(os, "this item does not exist")
-		
-		result_string = outputty.getvalue()
-		self.textplace.Clear()
-		self.textplace.ChangeValue(result_string)
-		
-				
-	def goUp(self, e):
-		global virtualitems
-		global CURRENTitem
-		global mdb
-		global modules
-		
-		
-		outputty = StringIO()
-		sys.stdout = outputty
-		
-		result = getParent(mdb, CURRENTitem)
+	def goUp(self):
+		# go to parent
+	
+		result = getParent(mdb, self.CURRENTitem)
 
 		if result == "error-item":
 			header(os, "no higher group exists")
 		else:
-			CURRENTitem = result
+			self.CURRENTitem = result
+	
+		self.show()
+	
+	## item functions
+	
+	def addItem(self):
 		
-		answer = showItem(mdb, modules, CURRENTitem, virtualitems)
-
-		result=answer[0]
-		
-		virtualitems = answer[1]
-
-		
-		if result == "error-item":
-			header(os, "this item does not exist")
-		
-		result_string = outputty.getvalue()
-		self.textplace.Clear()
-		self.textplace.ChangeValue(result_string)
-				
-		
-	def doReload(self, e):
-		global virtualitems
-		global CURRENTitem
-		global mdb
-		global modules
-		
-		outputty = StringIO()
-		sys.stdout = outputty
-		
-		answer = showItem(mdb, modules, CURRENTitem, virtualitems)
-
-		result=answer[0]
-		
-		virtualitems = answer[1]
-
-		
-		if result == "error-item":
-			header(os, "this item does not exist")
-		
-		result_string = outputty.getvalue()
-		self.textplace.Clear()
-		self.textplace.ChangeValue(result_string)
-		
-	def itemAdd(self, e):
-		global virtualitems
-		global CURRENTitem
-		global mdb
-		global modules
-		
-		outputty = StringIO()
-		sys.stdout = outputty
-		
-		addItem(mdb, modules, CURRENTitem, virtualitems)
+		addItem(mdb, modules, self.CURRENTitem, self.virtualitems)
 		result = updateItems(mdb, modules)
 		header(os, "item added, update affected: "+str(result)+" rows")
 		
-		result_string = outputty.getvalue()
-		self.textplace.Clear()
-		self.textplace.ChangeValue(result_string)
-		
-		
-	def itemRemove(self, e):
-		global virtualitems
-		global CURRENTitem
-		global mdb
-		global modules
-		
-		outputty = StringIO()
-		sys.stdout = outputty
-		
+	def removeItem(self):
 		# remove item
-		result = removeItem(mdb, modules, CURRENTitem)
-		if result == True:
-			header(os, "item removed")
-		elif result == "error-user":
-			header(os, "user canceled")
-		elif result == "error-server":
-			header(os, "error on server, try again")
-		else:
-			header(os, "something went wrong")	
-		
-		result_string = outputty.getvalue()
-		self.textplace.Clear()
-		self.textplace.ChangeValue(result_string)
-		
-		
-	def itemCheck(self, e):
-		global virtualitems
-		global CURRENTitem
-		global mdb
-		global modules
-		
-		outputty = StringIO()
-		sys.stdout = outputty
-		
-		result = updateItems(mdb, modules)
-		header(os, "update: "+str(result)+" rows removed")	
-		
-		result_string = outputty.getvalue()
-		self.textplace.Clear()
-		self.textplace.ChangeValue(result_string)
-	
-	
-	def doQuit(self, e):
-		self.Close
-	
-
-if __name__=='__main__':
-	app = wx.App()
-	Unify(None)
-	app.MainLoop()
-
-
-
-header(os)
-
-
-# integrate below into gui menu
-"""
-while True:
-	
-	answer = ask(mdb)
-	
-	header(os)
-	
-	
-	
-	## navigate commands
-	if answer == 'q':
-		# quit
-		break
-	
-	
-			
-	elif answer == 'g':
-		# show groups
-		showItemsAsTree(mdb, modules, 0, 0)
-			
-	elif answer == 's':
-		# show all
-		showItemsAsTree(mdb, modules)
-	
-	
-	
-	## item commands
-	elif answer == 'ia':
-		# add item
-		
-		addItem(mdb, modules, CURRENTitem, virtualitems)
-		result = updateItems(mdb, modules)
-		header(os, "item added, update affected: "+str(result)+" rows")
-		
-	elif answer == 'ir':
-		# remove item
-		result = removeItem(mdb, modules, CURRENTitem)
+		result = removeItem(mdb, modules, self.CURRENTitem)
 		if result == True:
 			header(os, "item removed")
 		elif result == "error-user":
@@ -297,17 +158,15 @@ while True:
 		else:
 			header(os, "something went wrong")	
 	
-	elif answer == 'iu':
+	def updateItem(self):
 		# update items
 		result = updateItems(mdb, modules)
 		header(os, "update: "+str(result)+" rows removed")	
 	
-	
-	
-	## widget commands
-	elif answer == 'wa':
+	## widget functions
+	def addWidget(self):
 		# add widget
-		result = addWidget(mdb, modules, CURRENTitem[0])
+		result = addWidget(mdb, modules, self.CURRENTitem[0])
 		
 		if result == True:
 			header(os, "Widget created and items added")
@@ -316,63 +175,28 @@ while True:
 		else:
 			header(os, "Something went wrong")
 	
-	elif answer == 'wr':
+	def removeWidget(self):
 		# remove widget
-		result = removeWidget(mdb, modules, CURRENTitem[0])
+		result = removeWidget(mdb, modules, self.CURRENTitem[0])
 		if result:
 			header(os, "Widget removed")
 		else:
 			header(os, "No widget removed")
 	
-	elif answer == 'wu':
+	def updateWidget(self):
 		# update or alter widget
-		result = alterWidget(mdb, modules, CURRENTitem[0])
+		result = alterWidget(mdb, modules, self.CURRENTitem[0])
 		
 		if result == True:
 			header(os, "Done with widget")
 		else:
 			header(os, "Somthing went wrong")
-			
-	else:
-		# all commands that show itemID, up or virtual id
-		
-		if answer == 'u':
-			# go to parent
 	
-			result = getParent(mdb, CURRENTitem)
-
-			if result == "error-item":
-				header(os, "no higher group exists")
-			else:
-				CURRENTitem = result
-				
-				
-		# if answer starts with v:
-		elif str(answer)[0] == 'v':
-			# requested virtual item
-			
-			# strip the v
-			CURRENTitem[1] = int(answer[1:]) 
-		elif answer == 'c':
-			# show current
-			pass	
-			
-		else:
-			# requested database item
-			CURRENTitem[0] = answer
-			CURRENTitem[1] = "none"
-		
-		answer = showItem(mdb, modules, CURRENTitem, virtualitems)
-
-		result=answer[0]
-		
-		virtualitems = answer[1]
-
-		
-		if result == "error-item":
-			header(os, "this item does not exist")
-# end while
-
-"""
+	
+	
+root = Tk()
+app = App(root)
+app.tkSimpleDialog = tkSimpleDialog
+root.mainloop()
 
 mdb.con.close()
